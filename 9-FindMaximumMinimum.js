c65776c77e1bb8b134d93c0ffef48cfc0426962f
GalagNathan <nathanchristiangalag@gmail.com>
2024-02-05 19:15:02 +0700

    challenge 9/366

---
+++ 9-FindMaximumMinimum.js
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/577a98a6ae28071780000989
+
+const min = (list) => Math.min(...list);
+
+const max = (list) => Math.max(...list);
+
+console.log(min([42, 54, 65, 87, 0]));
+console.log(max([42, 54, 65, 87, 0]));
