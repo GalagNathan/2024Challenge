d9d385c854c423b1507d1a8da368ea2b89be00f5
GalagNathan <nathanchristiangalag@gmail.com>
2024-03-05 19:25:13 +0700

    challenge 20/366

---
+++ 020-FeastOfManyBeasts.js
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/5aa736a455f906981800360d
+
+const feast = (beast, dish) =>
+  beast[0] === dish[0] && beast[beast.length - 1] === dish[dish.length - 1]
+    ? true
+    : false;
+
+console.log(feast("great blue heron", "garlic naan"));
