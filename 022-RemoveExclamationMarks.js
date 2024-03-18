bf4662fe1290e33af4288091d066301b60217360
GalagNathan <nathanchristiangalag@gmail.com>
2024-03-18 17:34:15 +0700

    challenge 22/366

---
+++ 022-RemoveExclamationMarks.js
@@ -0,0 +1,5 @@
+// https://www.codewars.com/kata/57a0885cbb9944e24c00008e/train/javascript
+
+const removeExclamationMarks = (s) => s.replaceAll("!", "");
+
+console.log(removeExclamationMarks("HG!!"));
